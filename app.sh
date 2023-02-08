39f9bb761bceef5a4618dd4f3b68c885de75ccd4
mchiusi <mchiusi@llruicms01.in2p3.fr>
2023-02-08 15:16:39 +0100

    requirement changes

--- app.sh
+++ app.sh
@@ -1,2 +1,2 @@
-python bye_splits/plot/display_plotly/main.py
+python bye_splits/plot/display_plotly/main.py --host 0.0.0.0 --port 8080
 #bokeh serve bye_splits/plot/display/ --address 0.0.0.0 --port 8080 --allow-websocket-origin=viz2-hgcal-event-display.app.cern.ch
